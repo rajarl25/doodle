7e94e5b2e0bff79bd2a5d654e79a4428e471d865
PKSS <sureshvel2k@gmail.com>
2019-05-19 18:39:46 +0530

    Doodle Pro Project Yuvaraj

--- DoodlePro/ClientApp/src/app/app.module.ts
+++ DoodlePro/ClientApp/src/app/app.module.ts
@@ -5,27 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    CreateAccountComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: CreateAccountComponent, pathMatch: 'full' },
+     
+      { path: 'create-account', component: CreateAccountComponent },
+      { path: 'login', component: LoginComponent },
     ])
   ],
   providers: [],
