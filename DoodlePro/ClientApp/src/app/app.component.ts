2be43ad95b6cde636ee292879da8c3a8a45b59bc
rajarl25 <rajaram25@gmail.com>
2019-05-22 21:47:17 +0530

    commited again rajaral25-1

--- DoodlePro/ClientApp/src/app/app.component.ts
+++ DoodlePro/ClientApp/src/app/app.component.ts
@@ -16,6 +16,6 @@ export class AppComponent {
     this.isLoggedIn = true;
     console.log('logged In Successfully-1');
     console.log('logged In Successfully-2');
-    console.log('committed by rajarl25');
+    console.log('committed by rajarl25-1');
   }
 }
